aff6344436bb79385961b4824e5b7803aa68734a
Andrew Thomas Lieberman <tuk40231@temple.edu>
2023-11-16 19:37:22 -0500

    start to edit,modify,delete for reservations

--- Project4/ViewReservations.aspx
+++ Project4/ViewReservations.aspx
@@ -14,8 +14,28 @@
         <asp:UpdatePanel ID="UpdatePanel1" runat="server">
             <ContentTemplate>
                 <div id="GridView">
-                    <asp:GridView ID="gvReservations" runat="server">
+                    <asp:GridView ID="gvReservations" runat="server" AutoGenerateColumns="false" OnRowCommand="gvReservations_RowCommand" OnRowDeleting="gvReservations_RowDeleting" OnRowUpdating="gvReservations_RowUpdating" DataKeyNames="Id">
+                        <Columns>
+                            <asp:TemplateField HeaderText="Modify">
+                                <ItemTemplate>
+                                    <asp:Button ID="btnModify" runat="server" Text="Modify" CommandName="Modify" CommandArgument='<%# Container.DataItemIndex %>' />
+                                </ItemTemplate>
+                                <EditItemTemplate>
+                                    <asp:TextBox ID="txtName" runat="server" Text='<%# Bind("Name") %>' />
+                                    <asp:TextBox ID="txtRestaurant" runat="server" Text='<%# Bind("Restaurant") %>' />
+                                    <asp:TextBox ID="txtDate" runat="server" Text='<%# Bind("Date") %>' />
+                                    <asp:TextBox ID="txtTime" runat="server" Text='<%# Bind("Time") %>' />
+                                    <asp:Button ID="btnUpdate" runat="server" Text="Update" CommandName="Update" CommandArgument='<%# Container.DataItemIndex %>' />
+                                </EditItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Delete">
+                                <ItemTemplate>
+                                    <asp:Button ID="btnDelete" runat="server" Text="Delete" CommandName="Delete" CommandArgument='<%# Eval("Id") %>' />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                        </Columns>
                     </asp:GridView>
+                    <asp:Label ID="lblConfirm" runat="server"></asp:Label>
                 </div>
                 <asp:Button ID="btnReturnToYourRestaurants" runat="server" Text="Return to Your Restaurants" OnClick="btnReturnToYourRestaurants_Click" BackColor="#3399FF" BorderStyle="Solid" Font-Size="Medium" />
             </ContentTemplate>
--- Project4/ViewReservations.aspx.cs
+++ Project4/ViewReservations.aspx.cs
@@ -48,6 +48,92 @@ namespace Project4
                 gvReservations.DataBind();
             }
         }
+
+        protected void gvReservations_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            SessionManagement sessionID = new SessionManagement();
+            string userID = sessionID.GetUserID();
+            if (e.CommandName == "Delete")
+            {
+                int reviewId = Convert.ToInt32(e.CommandArgument);
+
+                DeleteReview(reviewId);
+
+                string selectedName = userID;
+                PopulateReviews(selectedName);
+            }
+            else if (e.CommandName == "Modify")
+            {
+                int rowIndex = Convert.ToInt32(e.CommandArgument);
+                gvReservations.EditIndex = rowIndex;
+
+                string selectedName = userID;
+                PopulateReviews(selectedName);
+            }
+            else if (e.CommandName == "Update")
+            {
+                int rowIndex = Convert.ToInt32(e.CommandArgument);
+
+                if (rowIndex >= 0 && rowIndex < gvReservations.Rows.Count)
+                {
+                    GridViewRow editedRow = gvReservations.Rows[rowIndex];
+                    TextBox txtName = editedRow.FindControl("txtName") as TextBox;
+                    TextBox txtRestaurant = editedRow.FindControl("txtRestaurant") as TextBox;
+                    TextBox txtDate = editedRow.FindControl("txtDate") as TextBox;
+                    TextBox txtTime = editedRow.FindControl("txtTime") as TextBox;
+
+                    int restaurantId = Convert.ToInt32(gvReservations.DataKeys[rowIndex].Value);
+
+                    UpdateReservations(restaurantId, txtName.Text, txtRestaurant.Text, txtDate.Text, txtTime.Text);
+
+                    gvReservations.EditIndex = -1;
+                    string selectedName = userID;
+                    PopulateReviews(selectedName);
+                }
+            }
+        }
+
+        private void UpdateReservations(int restaurantId, string name, string restaurant, string date, string time)
+        {
+            DBConnect db = new DBConnect();
+
+            string storedProcedureName = "UpdateRestaurantForGV";
+
+            SqlCommand cmd = new SqlCommand(storedProcedureName);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@RestaurantId", restaurantId);
+            cmd.Parameters.AddWithValue("@Name", name);
+            cmd.Parameters.AddWithValue("@Restuarnt", restaurant);
+            cmd.Parameters.AddWithValue("@Date", date);
+            cmd.Parameters.AddWithValue("@Time", time);
+
+            db.DoUpdateUsingCmdObj(cmd);
+        }
+
+        protected void gvReservations_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            lblConfirm.Text = "Review successfully updated.";
+        }
+
+        protected void gvReservations_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            lblConfirm.Text = "Review successfully deleted.";
+        }
+
+        private void DeleteReview(int restaurantId)
+        {
+            DBConnect db = new DBConnect();
+
+            string storedProcedureName = "DeleteRestaurantReservation";
+
+            SqlCommand cmd = new SqlCommand(storedProcedureName);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@RestaurantId", restaurantId);
+
+            db.DoUpdateUsingCmdObj(cmd);
+        }
         protected void btnReturnToYourRestaurants_Click(object sender, EventArgs e)
         {
             Response.Redirect("Reviewer.aspx");
